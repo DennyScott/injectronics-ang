a7ae30aed45e3fc361e40c72998f2b0c273c3db2
Denny Scott <dennyscott301@gmail.com>
2014-07-15 09:50:40 -0500

    Added scrolling

--- app/scripts/controllers/partials/footer.js
+++ app/scripts/controllers/partials/footer.js
@@ -8,23 +8,24 @@
  * Controller for the header of the injectronics app.
  */
 angular.module('injectronicsApp')
-  .controller('FooterCtrl', function ($scope, $rootScope, $anchorScroll, $location) {
-		$scope.getFooter = function(){
-					if(typeof $rootScope.template === 'undefined' || typeof $rootScope.template.footer === 'undefined'){
-				return 'views/partials/footers/staticFooter.html';
-			}
-			if($rootScope.template.footer === 'staticFooter'){
-					return 'views/partials/footers/staticFooter.html';	
-			}else if($rootScope.template.header === 'none') {
-					return 'views/partials/footers/staticFooter.html';	
-			}else{
-				return 'views/partials/footers/staticFooter.html';
-			}
-				}	
-	
-		$scope.toTop = function() {
-			 $location.hash('top');
-        $anchorScroll();
-		}	
-  });
+.controller('FooterCtrl', function ($scope, $rootScope, $document) {
+	$scope.getFooter = function(){
+		if(typeof $rootScope.template === 'undefined' || typeof $rootScope.template.footer === 'undefined'){
+			return 'views/partials/footers/staticFooter.html';
+		}
+		if($rootScope.template.footer === 'staticFooter'){
+			return 'views/partials/footers/staticFooter.html';	
+		}else if($rootScope.template.header === 'none') {
+			return 'views/partials/footers/staticFooter.html';	
+		}else{
+			return 'views/partials/footers/staticFooter.html';
+		}
+	}	
+
+	$scope.toTop = function() {
+		var top = 0;
+		var duration = 2000; //milliseconds
+		$document.scrollTop(top, duration);
+	}	
+});
 
